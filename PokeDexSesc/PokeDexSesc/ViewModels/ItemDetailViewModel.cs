8e3a7461e42c34ae33749d7ecf184f854f2e7a02
Elton Naoki Iwakura <elton.naoki@gmail.com>
2020-03-27 18:01:49 -0400

    Envio do priimeiro projeto pokedex com xamarin forms

---
+++ PokeDexSesc/PokeDexSesc/ViewModels/ItemDetailViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using PokeDexSesc.Models;
+
+namespace PokeDexSesc.ViewModels
+{
+    public class ItemDetailViewModel : BaseViewModel
+    {
+        public Item Item { get; set; }
+        public ItemDetailViewModel(Item item = null)
+        {
+            Title = item?.Text;
+            Item = item;
+        }
+    }
+}
